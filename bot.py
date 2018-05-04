923d3414f4a0ad16e46500a732b1b0e199207439
schastie <am.vedova@gmail.com>
2018-05-04 16:56:30 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
-import os
+# -*- coding: utf-8 -*-
+
 import telebot
-  
-bot = telebot.TeleBot(os.environ['AAFYC9NrYLh3VH5DSdTIcu5u0Y9YxFRKbZY'])
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.reply_to(message, u"Hello, welcome to this bot!")
-bot.polling()
+
+bot = telebot.TeleBot('588205910:AAFYC9NrYLh3VH5DSdTIcu5u0Y9YxFRKbZY')
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+     bot.polling(none_stop=True)
