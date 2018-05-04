66159a967b006d0bce342b24e8630da9e689ae2a
schastie <am.vedova@gmail.com>
2018-05-04 16:44:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
 import telebot
   
-bot = telebot.TeleBot(os.environ['588205910:AAFYC9NrYLh3VH5DSdTIcu5u0Y9YxFRKbZY'])
+bot = telebot.TeleBot(os.environ['AAFYC9NrYLh3VH5DSdTIcu5u0Y9YxFRKbZY'])
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, u"Hello, welcome to this bot!")
