936d05b68cb9a66b1c1ba09878490933236571b4
schastie <am.vedova@gmail.com>
2018-05-09 22:44:21 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,24 +4,18 @@ import telebot
 import sqlite3, csv
 
 bot = telebot.TeleBot('588205910:AAFYC9NrYLh3VH5DSdTIcu5u0Y9YxFRKbZY')
-
+mestext = 'hello';
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
     bot.send_message(message.chat.id, message.text)
 
 if __name__ == '__main__':
     
-     con = sqlite3.connect('edinput.db')
-     cur = con.cursor()
-     cur.execute("CREATE TABLE inp (inpId, inpEd);") # use your column names here
-
-     with open('input.csv','rb') as fin: # `with` statement available in 2.5+
-      # csv.DictReader uses first line in file for column headings by default
-     dr = csv.DictReader(fin) # comma is default delimiter
-     to_db = [(i['col1'], i['col2']) for i in dr]
-
-     cur.executemany("INSERT INTO t (col1, col2) VALUES (?, ?);", to_db)
-     con.commit()
+     con = sqlite3.connect('input.sqlite')
+     cur.execute("select x from Table limit 1")
+     for row in cur:
+        print row
+        mestext = row;
      con.close()
         
      bot.polling(none_stop=True)
