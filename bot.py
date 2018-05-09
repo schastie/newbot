a545a602511ccce033d4ddc8474a29d403355805
schastie <am.vedova@gmail.com>
2018-05-09 23:15:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import telebot
-import sqlite3, csv
+import sqlite3
 
 bot = telebot.TeleBot('588205910:AAFYC9NrYLh3VH5DSdTIcu5u0Y9YxFRKbZY')
 mestext = 'hello';
 @bot.message_handler(content_types=["text"])
 def repeat_all_messages(message): # Название функции не играет никакой роли, в принципе
-    bot.send_message(message.chat.id, message.text)
+    bot.send_message(message.chat.id, mestext)
 
 if __name__ == '__main__':
     
